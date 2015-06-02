ec64437fff69da02ab2e0bd18d068e64410d8965
Alicia Lauerman <allydevour@me.com>
2015-06-02 19:08:25 -0400

    full label crud

--- src/models/label.js
+++ src/models/label.js
@@ -16,7 +16,15 @@ export default Model.extend(githubMixin, {
     editing: {
       type: 'boolean',
       default: false
+    },
+    saved: {
+      type: 'boolean',
+      default: true
     }
+  },
+
+  isNew () {
+    return !this.saved
   },
 
   update (newAttributes) {
