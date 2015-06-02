08970041154857b3610c8b6df10d914e5c90f3e6
Alicia Lauerman <allydevour@me.com>
2015-06-02 19:46:46 -0400

    pre-render layout and index

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
+require('babel/register')
 var getConfig = require('hjs-webpack');
+var Layout = require('./src/layout')
+var Public = require('./src/pages/public')
+var React = require('react')
 
 module.exports = getConfig({
   in: 'src/app.js',
   out: 'public',
   isDev: process.env.NODE_ENV !== 'production',
   html: function (context) {
+    var layoutHtml = React.renderToString(React.createElement(Layout, {me:{}}))
+    var publicHtml = React.renderToString(React.createElement(Public))
+
     return {
-      '200.html': context.defaultTemplate(),
-      'index.html': context.defaultTemplate()
+      '200.html': context.defaultTemplate({html: layoutHtml}),
+      'index.html': context.defaultTemplate({html: publicHtml})
     }
   }
 });
